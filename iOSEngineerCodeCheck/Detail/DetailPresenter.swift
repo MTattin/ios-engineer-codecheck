d4194ab8903c9cadfc800968e30705da712687df
tachikawa masakiyo <masakiyo_tattin@yahoo.co.jp>
2022-02-04 23:47:35 +0900

    Add new 2 files for dividing DetailViewController

---
+++ iOSEngineerCodeCheck/Detail/DetailPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  DetailPresenter.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Masakiyo Tachikawa on 2022/02/04.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
